49b0fa70e455829facf5a410cc0239f4c23f7bd6
paubt <pau_b@protonmail.com>
2021-05-31 23:04:23 +0200

    check if lin reg bodybrain goood

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 # This is a sample Python script.
 import pandas as pd
+from scipy import stats
 import matplotlib.pyplot as plt
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    headerList = ["i", "brain", "body"]
+    data = pd.read_fwf("/home/pau/PycharmProjects/ufz/mlPy/data/brainbodyratio.txt")
+    for col in data.columns:
+        print(col)
 
-
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    res = stats.linregress(data['brain'], data['body'])
+    print(res)
